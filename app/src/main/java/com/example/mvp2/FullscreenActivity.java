044e6f083da5308a346db63e7bea06a2916a00fb
aviralrabbit1 <aviral.robin@gmail.com>
2023-01-26 12:55:57 +0530

    main activity and 3 UIs

---
+++ app/src/main/java/com/example/mvp2/FullscreenActivity.java
@@ -0,0 +1,12 @@
+package com.example.mvp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+
+public class FullscreenActivity extends AppCompatActivity {
+
+}
