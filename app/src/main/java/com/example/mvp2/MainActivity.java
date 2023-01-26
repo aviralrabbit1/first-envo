616020cd3d187e4e16a70b67f69bcb00f7149e0e
aviralrabbit1 <aviral.robin@gmail.com>
2023-01-26 14:41:38 +0530

    trying to link second activity

--- app/src/main/java/com/example/mvp2/MainActivity.java
+++ app/src/main/java/com/example/mvp2/MainActivity.java
@@ -2,19 +2,21 @@ package com.example.mvp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button secondact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void onRegisterBtnClick (View view){
@@ -30,5 +32,17 @@ public class MainActivity extends AppCompatActivity {
         firstname.setText("First Name: " + editFirstName.getText().toString());
         lastname.setText("Last name: " + editLastName.getText().toString());
         gmail.setText("Email: " + editEmail.getText().toString());
+    }
+
+    public void OnClickButtonListener(){
+//        final Button buttonsecond = (Button) findViewById(R.id.buttontotwo);
+        secondact = findViewById(R.id.buttontotwo);
+        secondact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
