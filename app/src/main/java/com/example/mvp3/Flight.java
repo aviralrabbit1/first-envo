91d0a5c792c799c23adb79926f21bf9eb98aae1d
aviralrabbit1 <aviral.robin@gmail.com>
2023-01-30 15:47:05 +0530

    worked on flight buttons

--- app/src/main/java/com/example/mvp3/Flight.java
+++ app/src/main/java/com/example/mvp3/Flight.java
@@ -10,72 +10,86 @@ import android.widget.NumberPicker;
 import android.widget.TextView;
 
 
+import android.widget.TextView;
+
 public class Flight extends AppCompatActivity {
-
-    Button confirmflight;
-
-
-    TextView selectedNum, selectedTxt;
-    NumberPicker numberPicker, textPicker;
-
-
+    int minteger = 0;
+    int minteger2 = 0;
+    int minteger3 = 0;
+    int minteger4 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight);
-
-        confirmflight = (Button) findViewById(R.id.ConfirmFlight);
-        confirmflight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openDietActivity();
-            }
-        });
-
-
-        selectedNum = (TextView) findViewById(R.id.selectNumber);
-        selectedTxt = (TextView) findViewById(R.id.selectText);
-        numberPicker = (NumberPicker) findViewById(R.id.numPicker);
-        textPicker = (NumberPicker) findViewById(R.id.txtPicker);
-
-
-        //Populate NumberPicker values from minimum and maximum value range
-        numberPicker.setMinValue(0);
-        numberPicker.setMaxValue(10);
-        numberPicker.setWrapSelectorWheel(true);
-
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-
-                selectedNum.setText("Selected Number : " + i1);
-
-            }
-        });
-
-
-        //Populate NumberPicker values from String array values
-        final String[] values = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "JellyBean", "Kitkat", "Lollipop", "Marshmallow", "Nougat", "Oreo"};
-
-        textPicker.setMinValue(0);
-        textPicker.setMaxValue(values.length - 1);
-        textPicker.setDisplayedValues(values);
-        textPicker.setWrapSelectorWheel(true);
-
-        textPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-
-                selectedTxt.setText("Selected Text : " + values[i1]);
-
-            }
-        });
     }
 
-    public void openDietActivity(){
-        Intent intent = new Intent(this, Diet.class);
-        startActivity(intent);
+    public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
+
+    }public void decreaseInteger(View view) {
+        minteger = minteger - 1;
+        if(minteger<0){
+            minteger=0;
+        }
+        display(minteger);
     }
 
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.integer_number);
+        displayInteger.setText("" + number);
+    }
+    public void increaseInteger2(View view) {
+        minteger2 = minteger2 + 1;
+        display2(minteger2);
+
+    }public void decreaseInteger2(View view) {
+        minteger2 = minteger2 - 1;
+        if(minteger2<0){
+            minteger2=0;
+        }
+        display2(minteger2);
+    }
+
+    private void display2(int number) {
+        TextView displayInteger2 = (TextView) findViewById(
+                R.id.integer_number2);
+        displayInteger2.setText("" + number);
+    }
+    public void increaseInteger3(View view) {
+        minteger3 = minteger3 + 1;
+        display3(minteger3);
+
+    }public void decreaseInteger3(View view) {
+        minteger3 = minteger3 - 1;
+        if(minteger3<0){
+            minteger3=0;
+        }
+        display3(minteger3);
+    }
+
+    private void display3(int number) {
+        TextView displayInteger3 = (TextView) findViewById(
+                R.id.integer_number3);
+        displayInteger3.setText("" + number);
+    }
+    public void increaseInteger4(View view) {
+        minteger4 = minteger4 + 1;
+        display4(minteger4);
+
+    }public void decreaseInteger4(View view) {
+        minteger4 = minteger4 - 1;
+        if(minteger4<0){
+            minteger4=0;
+        }
+        display4(minteger4);
+    }
+
+    private void display4(int number) {
+        TextView displayInteger4 = (TextView) findViewById(
+                R.id.integer_number4);
+        displayInteger4.setText("" + number);
+    }
 }
 
--- app/src/main/java/com/example/mvp3/Result.java
+++ app/src/main/java/com/example/mvp3/Result.java
@@ -2,13 +2,78 @@ package com.example.mvp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
 
 public class Result extends AppCompatActivity {
+
+    Button confirmflight;
+
+
+    TextView selectedNum, selectedTxt;
+    NumberPicker numberPicker, textPicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        confirmflight = (Button) findViewById(R.id.ConfirmFlight);
+        confirmflight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDietActivity();
+            }
+        });
+
+
+        selectedNum = (TextView) findViewById(R.id.selectNumber);
+        selectedTxt = (TextView) findViewById(R.id.selectText);
+
+
+        //Populate NumberPicker values from minimum and maximum value range
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(10);
+        numberPicker.setWrapSelectorWheel(true);
+
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+
+                selectedNum.setText("Selected Number : " + i1);
+
+            }
+        });
+
+
+        //Populate NumberPicker values from String array values
+        final String[] values = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "JellyBean", "Kitkat", "Lollipop", "Marshmallow", "Nougat", "Oreo"};
+
+        textPicker.setMinValue(0);
+        textPicker.setMaxValue(values.length - 1);
+        textPicker.setDisplayedValues(values);
+        textPicker.setWrapSelectorWheel(true);
+
+        textPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+
+                selectedTxt.setText("Selected Text : " + values[i1]);
+
+            }
+        });
     }
+
+    public void openDietActivity(){
+        Intent intent = new Intent(this, Diet.class);
+        startActivity(intent);
+    }
+
 }
+
